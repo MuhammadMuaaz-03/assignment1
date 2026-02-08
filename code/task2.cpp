517bd90c794c5464b56ca66bb1a842b8ceac7476
MuhammadMuaaz-03 <m.muaazbm@gmail.com>
2026-02-08 20:01:31 +0500

    comit

---
+++ code/task2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+void add(int x, int y)
+{
+	cout << x + y << endl;
+}
+void add(double x, double y)
+{
+	cout << x + y << endl;
+}
+int main()
+{
+	double a, b;
+	cout << "Enter 2 numbers: ";
+	cin >> a >> b;
+}
